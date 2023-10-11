51dd1fe34397f1da1226285bc598dadf6512cca8
geznerd <geznerd@gmail.com>
2023-10-11 16:19:22 +0700

    Initial commit, set Column

---
+++ app/src/main/java/com/alqosbah/diceroller/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.alqosbah.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alqosbah.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+
+}
+
+
+@Composable
+fun DiceWithButtonImage(modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
