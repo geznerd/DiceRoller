d78f882fc8c894398f5aa8095dfe64298f3e6f2a
geznerd <geznerd@gmail.com>
2023-10-15 09:06:33 +0700

    adding drawables and roll logic

--- app/src/main/java/com/alqosbah/diceroller/MainActivity.kt
+++ app/src/main/java/com/alqosbah/diceroller/MainActivity.kt
@@ -3,23 +3,39 @@ package com.alqosbah.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alqosbah.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            DiceRollerApp()
 
         }
     }
@@ -27,7 +43,7 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun DiceRollerApp(){
+fun DiceRollerApp() {
     DiceWithButtonImage(modifier = Modifier
         .fillMaxSize()
         .wrapContentSize(Alignment.Center))
@@ -37,10 +53,37 @@ fun DiceRollerApp(){
 
 @Composable
 fun DiceWithButtonImage(modifier: Modifier = Modifier){
+    var result by remember { mutableStateOf(1)}
+    val imageResult = when(result){
+    1 -> R.drawable.dice_1
+    2 -> R.drawable.dice_2
+    3 -> R.drawable.dice_3
+    4 -> R.drawable.dice_4
+    5 -> R.drawable.dice_5
+    else -> R.drawable.dice_6
+}
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = result.toString(),
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold
+
+        )
+        Image(
+            painter = painterResource(
+                id = imageResult),
+                contentDescription = "1")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                result = (1..6).random()
+            }) {
+            Text(text = stringResource(id = R.string.roll_button))
+        }
 
     }
 }
